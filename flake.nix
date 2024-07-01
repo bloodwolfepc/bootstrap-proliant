9dfb7e192a60b95531ff1582d08f2932df510c0c
bloodwolfepc <bloodwolfepc@gmail.com>
2024-07-01 01:30:53 -0500

    commit

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {     
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/24.05";
 
     disko = {
       url = "github:nix-community/disko";
