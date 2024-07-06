2b7c71ff759cb3bbe521aeabc47b69d565b094fc
bloodwolfepc <bloodwolfepc@gmail.com>
2024-07-06 17:58:48 -0500

    commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,3 +16,25 @@ nixos-install --root /mnt --flake /mnt/etc/nixos#default
 #ssh-to-age -private-key -i $HOME/.ssh/id_ed25519 -o key.txt
 #sudo cp /persist/system/var/lib/sops-nix/key.txt /persist/home/bloodwolfe/.config/sops/key.txt
 #sudo chown -R $(id -u):$(id -g) /persist/home/bloodwolfe/.config/sops/age/key.txt
+#
+#
+#
+#
+#
+#$FLAKE=/home/bloodwolfe/projects/flake
+#
+#
+#mkdir -p /persist/system/var/lib/sops-nix/key.txt
+#ssh-to-age -private-key -i /persist/system/etc/ssh/ssh_host_ed25519_key -o /persist/system/var/lib/sops-nix/key.txt
+#mkdir -p /persist/$HOME/.config/sops
+#cp /persist/system/var/lib/sops-nix/key.txt /persist/$HOME/.config/sops/
+#
+#mkdir /persist/$FLAKE
+#cd /persist/$FLAKE
+#git clone https://github.com/bloodwolfepc/flake
+#cp /etc/nixos/hardware-configuration.nix /persist/$FLAKE/$HOSTNAME
+#
+#SSH TO AGE PUBKEY .sops
+#
+#sops updatekeys $FLAKE/secrets/*
+#CHOWN
